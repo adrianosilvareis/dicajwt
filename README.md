06794648fe59da17d02d8fd751ab213f2f05f4de
Adriano Reis <adriano_silvareis@hotmail.com>
2018-06-24 20:09:08 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# DICA JWT
+Curso rápido para utilização do JWT com Express
+
+[Traversy Media](https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA) - Canal do youtube
+
+[vídeo aula](https://www.youtube.com/watch?v=7nafaH9SddU) - Vídeo em inglês ensinando útilizar JWT
+
+---
+
+DICA's
+
+[Postman](https://www.getpostman.com/) - Ferramenta para testar rotas usando chrome
+
+[JSONWEBTOKEN](https://www.npmjs.com/package/jsonwebtoken) - Plugin para criação de token web
+
+[Documentation](https://github.com/auth0/node-jsonwebtoken) - Documentação do JWT
+
+
+---
+
+&copy; Adriano Reis - [MIT](LICENSE.md)
---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const app = express()
+
+
+app.get('/', (req,res,next) => {
+  res.json({
+    message: 'Welcome to the API'
+  })
+})
+
+app.post('/api/posts', verifyToken, (req, res) => {
+  res.json({
+    message: 'Post created...'
+  })
+})
+
+app.post('/api/login', (req, res) => {
+  //Mock user
+
+  const user = {
+    id: 1,
+    username: 'adriano',
+    email: 'adriano@email.com',
+  }
+
+  jwt.sign({ user }, 'secretkey', (err, token) => {
+    res.json({
+      token: token
+    })
+  })
+})
+
+//Format of Token
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+  //Get auth header value
+  const bearerHeader = req.headers['authorization']
+  //Check if bearer is undefined
+  if (bearerHeader) {
+    //Split at the space
+    const bearer = bearerHeader.split(' ')
+    //Get token from array
+    const bearerToken = bearer[1]
+    //Set the token
+    req.token = bearerToken
+    //next middleware
+    next()
+  } else {
+    //Forbidden
+    res.sendStatus(403)
+  }
+}
+
+app.listen(3000, () => console.log('Server started on port 3000'))
